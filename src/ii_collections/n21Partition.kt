ae6f3eb882c420af6a6a26c0bb0fba380e6b106c
shuto4ka <noga?1virta>
2019-08-31 14:05:06 +0300

    collections

--- src/ii_collections/n21Partition.kt
+++ src/ii_collections/n21Partition.kt
@@ -12,5 +12,8 @@ fun example8() {
 
 fun Shop.getCustomersWithMoreUndeliveredOrdersThanDelivered(): Set<Customer> {
     // Return customers who have more undelivered orders than delivered
-    todoCollectionTask()
+    return customers.filter { customer ->
+        val (delivered, undelivered) = customer.orders.partition { it.isDelivered }
+        delivered.size < undelivered.size
+    }.toSet()
 }
